8bd20be07f9f97d73df99a078c4b30bbf11457a6
MiguelElu <miguel.elu.lanzagorta@gmail.com>
2023-06-14 19:17:03 -0600

    add class load file usuarios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,88 @@
 package org.example;
 
+import java.io.*;
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static HashMap<String,String> login = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+         load();
+
+
+
     }
+
+    public static void load() throws IOException {
+        boolean success = true;
+        while (success) {
+            String inputFilename = "C:\\Users\\migue\\OneDrive - Universidad Tecmilenio\\Tetramestre 5\\Certificacion de Java 1\\EvidFinal\\db\\usuarios_contrasenas.txt";
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new FileReader(inputFilename));
+
+
+                String line;
+                String user = null;
+                String pass = null;
+                boolean primero = true;
+                String actual = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                    primero = true;
+                    user = "";
+                    pass = "";
+                    for (int i = 0; i < line.length(); i++) {
+                        actual = String.valueOf(line.charAt(i));
+                        if (actual.equals(",")) {
+                            i = i + 1;
+                            primero = false;
+                        }
+                        if (primero) {
+                            user = user + line.charAt(i);
+
+                        } else {
+                            pass = pass + line.charAt(i);
+                        }
+                    }
+                    login.put(user, pass);
+                    System.out.println(user + pass);
+                    success = false;
+
+                }
+                // bufferedWriter.write(line + "\n");
+
+            } catch (IOException e) {
+                System.out.println("IOException catched while reading: " + e.getMessage());
+                String f = "usuarios_contrasenas.txt";
+                new File("C:\\Users\\migue\\OneDrive - Universidad Tecmilenio\\Tetramestre 5\\Certificacion de Java 1\\EvidFinal\\db", f).createNewFile();
+                String outputFilename = "C:\\Users\\migue\\OneDrive - Universidad Tecmilenio\\Tetramestre 5\\Certificacion de Java 1\\EvidFinal\\db\\usuarios_contrasenas.txt";
+                BufferedWriter bufferedWriter = null;
+
+                try {
+
+                    bufferedWriter = new BufferedWriter(new FileWriter(outputFilename));
+
+
+                    bufferedWriter.write("admin,admin\n");
+
+
+                } catch (IOException e2) {
+                    System.out.println("IOException catched while reading: " + e2.getMessage());
+                } finally {
+                    try {
+
+                        if (bufferedWriter != null) {
+                            bufferedWriter.close();
+                        }
+                    } catch (IOException e2) {
+                        System.out.println("IOException catched while closing: " + e.getMessage());
+                    }
+                }
+
+            }
+
+        }
+    }
+
 }
