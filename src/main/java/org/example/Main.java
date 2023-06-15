e3f4a5c070e73d2e414998b7a0d57fb777013a96
MiguelElu <miguel.elu.lanzagorta@gmail.com>
2023-06-14 19:29:20 -0600

    add class load file doctores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
          load();
+         loadd();
 
 
 
@@ -65,6 +66,86 @@ public class Main {
 
 
                     bufferedWriter.write("admin,admin\n");
+
+
+                } catch (IOException e2) {
+                    System.out.println("IOException catched while reading: " + e2.getMessage());
+                } finally {
+                    try {
+
+                        if (bufferedWriter != null) {
+                            bufferedWriter.close();
+                        }
+                    } catch (IOException e2) {
+                        System.out.println("IOException catched while closing: " + e.getMessage());
+                    }
+                }
+
+            }
+
+        }
+    }
+    public static void loadd() throws IOException {
+        boolean success = true;
+        while (success) {
+            String inputFilename = "C:\\Users\\migue\\OneDrive - Universidad Tecmilenio\\Tetramestre 5\\Certificacion de Java 1\\EvidFinal\\db\\doctores.txt";
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new FileReader(inputFilename));
+
+
+                String line;
+                String id = null;
+                String nom = null;
+                String esp = null;
+                boolean primero = true;
+                boolean segundo = true;
+                String actual = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                    primero = true;
+                    segundo = true;
+                    id = "";
+                    nom = "";
+                    esp = "";
+                    for (int i = 0; i < line.length(); i++) {
+                        actual = String.valueOf(line.charAt(i));
+                        if (actual.equals(",")) {
+                            if (primero) {
+                                i = i + 1;
+                                primero = false;
+                            } else {
+                                i = i + 1;
+                                segundo = false;
+                            }
+                        }
+                        if (primero&&segundo) {
+                            id = id + line.charAt(i);
+
+                        } else if (segundo){
+                            nom = nom + line.charAt(i);
+                        }
+                        else esp = esp +line.charAt(i);
+                    }
+
+                    System.out.println(id+" "+ nom +" "+ esp);
+                    success = false;
+
+                }
+                // bufferedWriter.write(line + "\n");
+
+            } catch (IOException e) {
+                System.out.println("IOException catched while reading: " + e.getMessage());
+                String f = "doctores.txt";
+                new File("C:\\Users\\migue\\OneDrive - Universidad Tecmilenio\\Tetramestre 5\\Certificacion de Java 1\\EvidFinal\\db", f).createNewFile();
+                String outputFilename = "C:\\Users\\migue\\OneDrive - Universidad Tecmilenio\\Tetramestre 5\\Certificacion de Java 1\\EvidFinal\\db\\doctores.txt";
+                BufferedWriter bufferedWriter = null;
+
+                try {
+
+                    bufferedWriter = new BufferedWriter(new FileWriter(outputFilename));
+
+
+                    bufferedWriter.write("d1,ejemplo,ejemplo\n");
 
 
                 } catch (IOException e2) {
